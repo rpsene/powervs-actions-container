4f8a2521c7ea84d3ec3457a04d8c693b3ddc1208
Rafael Peria de Sene <rpsene@br.ibm.com>
2021-03-09 20:31:06 -0300

    Shellcheck fixes.
    Signed-off-by: Rafael Peria de Sene <rpsene@br.ibm.com>

--- scripts/actions.sh
+++ scripts/actions.sh
@@ -11,8 +11,8 @@ set -o pipefail
 
 function check_connectivity() {
 
-    curl --output /dev/null --silent --head --fail http://cloud.ibm.com
-    if [ ! $? -eq 0 ]; then
+    if ! curl --output /dev/null --silent --head --fail http://cloud.ibm.com -eq 0
+    then
         echo
         echo "ERROR: Please, check your internet connection."
         echo "       It was not possible reach IBM Cloud."
@@ -25,13 +25,12 @@ function create(){
     local CLUSTER_OCP_VERSION=$1
     local CLUSTER_REQUESTOR_EMAIL=$2
 
-    curl -sS --fail -X POST "$PVS_JENKINS_JOB_URL" \
+    if curl -sS --fail -X POST "$PVS_JENKINS_JOB_URL" \
     --user "$PVS_JENKINS_USER":"$PVS_JENKINS_TOKEN" \
     --data API_KEY="$PVS_API_KEY" \
     --data REQUESTOR_EMAIL="$CLUSTER_REQUESTOR_EMAIL" \
     --data OPENSHIFT_VERSION="$CLUSTER_OCP_VERSION"
-
-    if [ $? -eq 0 ]; then
+    then
         echo
         echo "SUCCESS: job was created and you will be notified at $CLUSTER_REQUESTOR_EMAIL."
         echo
@@ -46,13 +45,12 @@ function destroy(){
 
     local OCP_CLUSTER_ID=$1
 
-    curl -sS --fail -X POST "$PVS_JENKINS_JOB_URL" \
+    if curl -sS --fail -X POST "$PVS_JENKINS_JOB_URL" \
     --user "$PVS_JENKINS_USER":"$PVS_JENKINS_TOKEN" \
     --data REQUESTOR_EMAIL="$CLUSTER_REQUESTOR_EMAIL" \
     --data API_KEY="$PVS_API_KEY" \
     --data CLUSTER_ID="$OCP_CLUSTER_ID"
-
-    if [ $? -eq 0 ]; then
+    then
         echo
         echo "SUCCESS: job was created and you will be notified at $CLUSTER_REQUESTOR_EMAIL."
         echo
@@ -69,29 +67,24 @@ function run (){
 
     local ACTIONS=("create" "destroy")
 
-	if [ -z $ACTION ]; then
-        echo
-        echo "ERROR: please, select one of the supported actions ${ACTIONS[@]}."
-        echo "       ./upi-conf-powervs-commands.sh ACTION OCP_VERSION REQUESTOR_EMAIL"
-        echo
+	if [ -z "$ACTION" ]; then
+        printf "ERROR: %s\n" "select one of the supported actions: " "${ACTIONS[@]}"
 		exit 1
-    elif [[ ! " ${ACTIONS[@]} " =~ " ${ACTION} " ]]; then
-        echo
-        echo "ERROR: this action is not supported."
-        echo "       pick one of the following ${ACTIONS[@]}."
-        echo "       ./upi-conf-powervs-commands.sh ACTION OCP_VERSION REQUESTOR_EMAIL"
-        echo
+    else
+        for action in "${ACTIONS[@]}"; do
+        [[ ! "$ACTION" = "$action" ]] && echo "This action ($ACTION) is not supported."
         exit 1
+        done
     fi
     if [[ "$ACTION" == "create" ]]; then
-        if [ -z $CLUSTER_OCP_VERSION ]; then
+        if [ -z "$CLUSTER_OCP_VERSION" ]; then
 		    echo
 		    echo "ERROR: please, set the RedHat OpenShift version."
             echo "       ./upi-conf-powervs-commands.sh ACTION OCP_VERSION REQUESTOR_EMAIL"
 		    echo
 		    exit 1
         else
-            if [ -z $CLUSTER_REQUESTOR_EMAIL ]; then
+            if [ -z "$CLUSTER_REQUESTOR_EMAIL" ]; then
 		        echo
 		        echo "ERROR: please, set the email of the cluster requestor."
                 echo "       ./upi-conf-powervs-commands.sh ACTION OCP_VERSION REQUESTOR_EMAIL"
@@ -102,7 +95,7 @@ function run (){
             fi
         fi
 	elif [[ "$ACTION" == "destroy" ]]; then
-        if [ -z $CLUSTER_ID ]; then
+        if [ -z "$CLUSTER_ID" ]; then
 		    echo
 		    echo "ERROR: please, set the cluster you want to destroy."
 		    echo "       ./upi-conf-powervs-commands.sh ACTION CLUSTER_ID CLUSTER_REQUESTOR_EMAIL"
